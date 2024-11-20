91660dbf7e055b3efbdd4816131f27f8d4751573
t <mtanmoy5086@gmail.com>
2024-11-20 08:47:45 +0000

    prime update jolt

--- jolt/algos/nth_prime/native_jolt/guest/src/lib.rs
+++ jolt/algos/nth_prime/native_jolt/guest/src/lib.rs
@@ -1,6 +1,20 @@
 #![no_main]
 
-use wasm::nth_prime;
+#[no_mangle]
+pub fn nth_prime(n: u64) -> u64 {
+    if n == 0 {
+        return 0;
+    }
+    let mut primes = vec![2];
+    let mut candidate = 3;
+    while primes.len() < n as usize {
+        if primes.iter().all(|&p| candidate % p != 0) {
+            primes.push(candidate);
+        }
+        candidate += 2;
+    }
+    primes[n as usize - 1]
+}
 
 #[jolt::provable(stack_size = 100_000, memory_size = 10_000_000, max_input_size = 10000000)]
 fn nth_prime_wrapper(n: u64) -> u64 {
