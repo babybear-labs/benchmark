3d3c82306def0dc22052fa7872f938eb3fc8fda5
t <mtanmoy5086@gmail.com>
2024-11-20 08:33:42 +0000

    cleanup jolt

--- jolt/algos/ecdsa_verify/native_jolt/guest/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-
-use wasm::ecdsa_verify_n;
-
-#[jolt::provable(stack_size = 1000_000, memory_size = 10000000, max_input_size = 10000000)]
-fn ecdsa_verify_n_wrapper(n: u32) {
-    ecdsa_verify_n(n);
-}
