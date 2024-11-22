c6eb9cb02d0cfa553d4b473f86cf56bced22d4c6
Michael Zhu <mchl.zhu.96@gmail.com>
2024-11-22 12:34:40 -0500

    Fix Jolt benchmarks

--- jolt/algos/addition/src/main.rs
+++ jolt/algos/addition/src/main.rs
@@ -2,12 +2,14 @@ use jolt::Serializable;
 
 pub fn main() {
     let start = std::time::Instant::now();
-    let proving_time = std::time::Instant::now();
     let (prove_add, verify_add) = guest::build_add();
+    let proving_time = std::time::Instant::now();
     let (output, proof) = prove_add(10, 20);
     println!("Prover Time {:?}", proving_time.elapsed());
 
-    proof.save_to_file("proof.bin").expect("Failed to save proof to file");
+    proof
+        .save_to_file("proof.bin")
+        .expect("Failed to save proof to file");
     println!("Proof Size {:?}", proof.size().unwrap());
     println!();
 
