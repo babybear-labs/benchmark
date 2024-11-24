975c9a7a675cc6f84c6ee64c337b3ea0c8d235aa
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2024-11-24 19:09:34 +0600

    jolt arithmatic bench update

--- jolt/algos/addition/src/main.rs
+++ jolt/algos/addition/src/main.rs
@@ -2,24 +2,29 @@ use jolt::Serializable;
 
 pub fn main() {
     let start = std::time::Instant::now();
-    let (prove_add, verify_add) = guest::build_add();
+
+    let srs_compute_and_guest_program_compile_time = std::time::Instant::now();
+    let (prove, verify) = guest::build_add();
+    println!("SRS Compute and Guest Program Compile Time {:?}", srs_compute_and_guest_program_compile_time.elapsed());
+    println!();
+
     let proving_time = std::time::Instant::now();
-    let (output, proof) = prove_add(10, 20);
+    let (output, proof) = prove(10, 20);
     println!("Prover Time {:?}", proving_time.elapsed());
 
-    proof
-        .save_to_file("proof.bin")
-        .expect("Failed to save proof to file");
+    // proof
+    //     .save_to_file("proof.bin")
+    //     .expect("Failed to save proof to file");
     println!("Proof Size {:?}", proof.size().unwrap());
     println!();
 
     let verify_time = std::time::Instant::now();
-    let is_valid = verify_add(proof);
+    let is_valid = verify(proof);
     println!("Verify Time {:?}", verify_time.elapsed());
     println!();
 
-    println!("result: {:?}", output);
-    println!("valid: {}", is_valid);
+    // println!("result: {:?}", output);
+    println!("isProofValid: {}", is_valid);
 
-    println!("Total Time elapsed: {:?}", start.elapsed());
+    println!("Total Time Elapsed: (build + prove + verify) {:?}", start.elapsed());
 }
--- jolt/algos/subtraction/src/main.rs
+++ jolt/algos/subtraction/src/main.rs
@@ -2,24 +2,29 @@ use jolt::Serializable;
 
 pub fn main() {
     let start = std::time::Instant::now();
-    let (prove_sub, verify_sub) = guest::build_sub();
+
+    let srs_compute_and_guest_program_compile_time = std::time::Instant::now();
+    let (prove, verify) = guest::build_sub();
+    println!("SRS Compute and Guest Program Compile Time {:?}", srs_compute_and_guest_program_compile_time.elapsed());
+    println!();
+
     let proving_time = std::time::Instant::now();
-    let (output, proof) = prove_sub(100, 10);
+    let (output, proof) = prove(100, 10);
     println!("Prover Time {:?}", proving_time.elapsed());
 
-    proof
-        .save_to_file("proof.bin")
-        .expect("Failed to save proof to file");
+    // proof
+    //     .save_to_file("proof.bin")
+    //     .expect("Failed to save proof to file");
     println!("Proof Size {:?}", proof.size().unwrap());
     println!();
 
     let verify_time = std::time::Instant::now();
-    let is_valid = verify_sub(proof);
+    let is_valid = verify(proof);
     println!("Verify Time {:?}", verify_time.elapsed());
     println!();
 
-    println!("result: {:?}", output);
-    println!("valid: {}", is_valid);
+    // println!("result: {:?}", output);
+    println!("isProofValid: {}", is_valid);
 
-    println!("Total Time elapsed: {:?}", start.elapsed());
+    println!("Total Time Elapsed: (build + prove + verify) {:?}", start.elapsed());
 }
